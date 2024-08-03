88b8cdb40d7db6b22c889c31eaf8772166f98390
Vsevolod Pletnev <vseplet@gmail.com>
2024-08-04 00:35:43 +0700

    deploy to jsr

--- source/types.ts
+++ source/types.ts
@@ -7,3 +7,11 @@ export type PromiseNullOr<T> = Promise<NullOr<T>>;
 export type ErrorOr<T> = Error | T;
 
 export type PromiseErrorOr<T> = Promise<ErrorOr<T>>;
+
+export type Constructor<C> = new () => C;
+
+export type Tail<T extends any[]> = T extends [any, ...infer Rest] ? Rest
+  : never;
+
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false;
